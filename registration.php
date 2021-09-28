280ffd7b616669adaeaf5887311efb0c48ddecff
Wishusucess <90965189+Wishusucess@users.noreply.github.com>
2021-09-29 00:53:18 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Source      Wishusucess
+ * package     Wishusucess_CustomerAccount
+ * Author      Wishusucess Magento Development Team
+ * website     https://www.wishusucess.com/
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Wishusucess_CustomerAccount',
+    __DIR__
+);
